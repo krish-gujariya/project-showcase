df7753896d8351b4742b81d3a29001e1d146bd98
krish.gujariya <krish.gujariya.2024@gmail.com>
2024-04-03 20:01:49 +0530

    integrates routing

--- Server.js
+++ Server.js
@@ -2,14 +2,6 @@ const exp = require("express");
 var bds = require("body-parser");
 let app = exp();
 
-const controls = require("./controller/Controls");
-const paging = require("./controller/Paggination");
-const result = require("./controller/resultcontrol");
-const attendance = require("./controller/attendance-controll");
-const sql = require("./controller/sqlquerry-controll");
-const sort = require("./controller/sorting-record");
-const search = require("./controller/searchingdata-controll");
-const delimeter = require("./controller/delimeter-search");
 const {routes} = require("./routes/routes");
 
 app.set("view engine", "ejs");
